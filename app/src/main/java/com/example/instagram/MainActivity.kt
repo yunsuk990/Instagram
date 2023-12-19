a1f060a56c26ff1a585367c7876dc8e046b98253
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2023-12-19 14:51:45 +0900

    Conner Update MainActivity

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.instagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//conner
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
