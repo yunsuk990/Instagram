7fac528b0d2af55070e24945254075241ecd2103
yunsuk990 <yunsuk990@naver.com>
2023-12-19 19:39:55 +0900

    Create Fragment

---
+++ app/src/main/java/com/example/instagram/PostFragment.kt
@@ -0,0 +1,20 @@
+package com.example.instagram
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.instagram.databinding.FragmentPostBinding
+
+class PostFragment : Fragment() {
+    lateinit var binding: FragmentPostBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPostBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
