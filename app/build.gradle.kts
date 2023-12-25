305a463b9992274971a3ecca54c4ff0594789ec5
fizzyseo <whatafineday23@gmail.com>
2023-12-25 21:57:34 +0900

    의존성 추가
    add GSON, viewpager2, Retrofit, okHttp, Glide implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,23 @@ dependencies {
 
     //recyclerview 의존성 추가
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+    // GSON
+    implementation ("com.google.code.gson:gson:2.8.5")
+
+    // viewpager2
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    // okHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
 }
